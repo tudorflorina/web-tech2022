36411d7d8c9af79d35de419d3c4b7ddbc01d1d26
Florina <tudorflorina20@stud.ase.ro>
2022-10-08 20:00:24 +0000

    pasul 4

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var http = require("http");
+const express = require("express");
 
-//create a server object:
-http
-  .createServer(function (req, res) {
-    res.write("Hello Web!"); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+const app = express();
+
+app.get("http://server/ping", (req, res) => {
+  res.send("pong");
+});
+
+app.listen(8080);
